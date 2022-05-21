45a416c59ce6a4dbf6de550ae2fd2a8a78aa3951
Enzo <enzo.baldisserri@outlook.fr>
2022-05-21 13:35:22 +0200

    chore: add some tests to contexts, fix them if needed (#5)
    * test: add for accounts
    
    * test: add for auth

---
+++ apps/anacounts/test/support/fixtures/accounts_fixtures.ex
@@ -0,0 +1,33 @@
+defmodule Anacounts.AccountsFixtures do
+  @moduledoc """
+  Fixtures for the `Accounts` context
+  """
+
+  def valid_book_name, do: "A valid book name !"
+  def invalid_book_name, do: nil
+
+  def valid_book_attributes(attrs \\ %{}) do
+    Map.merge(attrs, %{
+      name: valid_book_name()
+    })
+  end
+
+  def invalid_book_attributes(attrs \\ %{}) do
+    Map.merge(attrs, %{
+      name: invalid_book_name()
+    })
+  end
+
+  def book_fixture(user, attrs \\ %{}) do
+    {:ok, book} =
+      attrs
+      |> valid_book_attributes()
+      |> then(&Anacounts.Accounts.create_book(user, &1))
+
+    book
+  end
+
+  def setup_book_fixture(%{user: user} = context) do
+    Map.put(context, :book, book_fixture(user))
+  end
+end
