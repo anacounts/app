0daf1d34aa334ab6fd665f4d4d536b217b415841
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2022-07-15 22:49:53 +0200

    feat: create Balance.UserParams

---
+++ apps/anacounts/priv/repo/migrations/20220715193047_create_balance_user_params_indexes.exs
@@ -0,0 +1,11 @@
+defmodule Anacounts.Repo.Migrations.CreateBalanceUserParamsIndexes do
+  use Ecto.Migration
+  @disable_ddl_transaction true
+  @disable_migration_lock true
+
+  def change do
+    create index(:balance_user_params, :user_id, concurrently: true)
+    create index(:balance_user_params, :means_code, concurrently: true)
+    create unique_index(:balance_user_params, [:user_id, :means_code], concurrently: true)
+  end
+end
--- apps/anacounts/test/support/fixtures/accounts/balance_fixtures.ex
+++ apps/anacounts/test/support/fixtures/accounts/balance_fixtures.ex
@@ -3,6 +3,8 @@ defmodule Anacounts.Accounts.BalanceFixtures do
   Fixtures for the `Accounts.Balance` context
   """
 
+  alias Anacounts.Accounts.Balance
+
   def valid_balance_means_code, do: :divide_equally
   def valid_balance_params, do: %{}
 
@@ -11,5 +13,31 @@ defmodule Anacounts.Accounts.BalanceFixtures do
       means_code: valid_balance_means_code(),
       params: valid_balance_params()
     })
+  end
+
+  def valid_balance_user_params_attrs(attrs \\ %{}) do
+    Enum.into(attrs, %{
+      means_code: valid_balance_means_code(),
+      params: valid_balance_params()
+    })
+  end
+
+  @base_user_params_fixtures [
+    %{means_code: :divide_equally, params: %{}}
+  ]
+
+  def balance_user_params_fixtures(user) do
+    for base_params <- @base_user_params_fixtures do
+      {:ok, user_params} =
+        base_params
+        |> Map.put(:user_id, user.id)
+        |> Balance.upsert_user_params()
+
+      user_params
+    end
+  end
+
+  def setup_balance_user_params_fixtures(%{user: user} = context) do
+    Map.put(context, :balance_user_params, balance_user_params_fixtures(user))
   end
 end
