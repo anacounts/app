56e518f45b6107b4c592071bdfdbe10c190345e0
Enzo <enzo.baldisserri@outlook.fr>
2022-07-22 19:30:14 +0200

    fix: compute transactions (#28)
    * fix: compute transactions
    
    * style: split function for less complexity

--- apps/anacounts/lib/anacounts/accounts/balance.ex
+++ apps/anacounts/lib/anacounts/accounts/balance.ex
@@ -73,9 +73,75 @@ defmodule Anacounts.Accounts.Balance do
     Graph.nodes_weight(balance_graph)
   end
 
-  defp transactions(_members_balance) do
-    # TODO
-    []
+  defp transactions(members_balance) do
+    {debtors, creditors} =
+      Enum.split_with(members_balance, fn {_member_id, amount} -> Money.negative?(amount) end)
+
+    make_transactions(debtors, creditors, [])
+  end
+
+  # Creates necessary transactions between creditors and debitors
+  # to balance things. The total sum of creditors and debitors should be
+  # equal to 0, or the function will crash.
+  defp make_transactions(creditors, debitors, transactions)
+  defp make_transactions([], [], transactions), do: transactions
+
+  defp make_transactions(
+         [{_debtor, neg_debt} | _other_debtors] = all_debtors,
+         [{_creditor, credit} | _other_creditors] = all_creditors,
+         transactions
+       ) do
+    debt = Money.neg(neg_debt)
+
+    Money.cmp(credit, debt)
+    |> add_transaction_from_cmp(all_debtors, all_creditors, transactions)
+  end
+
+  defp add_transaction_from_cmp(
+         :eq,
+         [{debtor, neg_debt} | other_debtors],
+         [{creditor, _credit} | other_creditors],
+         transactions
+       ) do
+    debt = Money.neg(neg_debt)
+    new_transaction = %{from: debtor, to: creditor, amount: debt}
+
+    make_transactions(
+      other_debtors,
+      other_creditors,
+      [new_transaction | transactions]
+    )
+  end
+
+  defp add_transaction_from_cmp(
+         :gt,
+         [{debtor, neg_debt} | other_debtors],
+         [{creditor, credit} | other_creditors],
+         transactions
+       ) do
+    debt = Money.neg(neg_debt)
+    new_transaction = %{from: debtor, to: creditor, amount: debt}
+
+    make_transactions(
+      other_debtors,
+      [{creditor, Money.subtract(credit, debt)} | other_creditors],
+      [new_transaction | transactions]
+    )
+  end
+
+  defp add_transaction_from_cmp(
+         :lt,
+         [{debtor, neg_debt} | other_debtors],
+         [{creditor, credit} | other_creditors],
+         transactions
+       ) do
+    new_transaction = %{from: debtor, to: creditor, amount: credit}
+
+    make_transactions(
+      [{debtor, Money.add(neg_debt, credit)} | other_debtors],
+      other_creditors,
+      [new_transaction | transactions]
+    )
   end
 
   # --- Actual module content ---
--- apps/anacounts/test/lib/accounts/balance_test.exs
+++ apps/anacounts/test/lib/accounts/balance_test.exs
@@ -11,9 +11,6 @@ defmodule Anacounts.Accounts.BalanceTest do
 
   alias Anacounts.Repo
 
-  # TODO reenable for_book/1 test
-
-  @tag :skip
   describe "for_book/1" do
     setup :setup_user_fixture
     setup :setup_book_fixture
@@ -32,15 +29,101 @@ defmodule Anacounts.Accounts.BalanceTest do
 
       assert Balance.for_book(book.id) == %{
                members_balance: %{
-                 member.id => %Money{amount: 5, currency: :EUR},
-                 other_member.id => %Money{amount: -5, currency: :EUR}
+                 member.id => Money.new(5, :EUR),
+                 other_member.id => Money.new(-5, :EUR)
                },
                transactions: [
                  %{
-                   amount: %Money{amount: 5, currency: :EUR},
+                   amount: Money.new(5, :EUR),
                    from: other_member.id,
                    to: member.id
                  }
+               ]
+             }
+    end
+
+    test "balances multiple transfers correctly #1", %{book: %{members: [member1]} = book} do
+      member2 = book_member_fixture(book, user_fixture())
+      member3 = book_member_fixture(book, user_fixture())
+      member4 = book_member_fixture(book, user_fixture())
+
+      _transfer1 =
+        money_transfer_fixture(
+          amount: Money.new(400, :EUR),
+          book_id: book.id,
+          tenant_id: member1.id,
+          peers: [
+            %{member_id: member1.id},
+            %{member_id: member2.id},
+            %{member_id: member3.id},
+            %{member_id: member4.id}
+          ]
+        )
+
+      _transfer2 =
+        money_transfer_fixture(
+          amount: Money.new(400, :EUR),
+          book_id: book.id,
+          tenant_id: member2.id,
+          peers: [
+            %{member_id: member1.id},
+            %{member_id: member2.id},
+            %{member_id: member3.id},
+            %{member_id: member4.id}
+          ]
+        )
+
+      assert Balance.for_book(book.id) == %{
+               members_balance: %{
+                 member1.id => Money.new(200, :EUR),
+                 member2.id => Money.new(200, :EUR),
+                 member3.id => Money.new(-200, :EUR),
+                 member4.id => Money.new(-200, :EUR)
+               },
+               transactions: [
+                 %{from: member4.id, to: member2.id, amount: Money.new(200, :EUR)},
+                 %{from: member3.id, to: member1.id, amount: Money.new(200, :EUR)}
+               ]
+             }
+    end
+
+    test "balances multiple transfers correctly #2", %{book: %{members: [member1]} = book} do
+      member2 = book_member_fixture(book, user_fixture())
+      member3 = book_member_fixture(book, user_fixture())
+
+      _transfer1 =
+        money_transfer_fixture(
+          amount: Money.new(300, :EUR),
+          book_id: book.id,
+          tenant_id: member1.id,
+          peers: [
+            %{member_id: member1.id},
+            %{member_id: member2.id},
+            %{member_id: member3.id}
+          ]
+        )
+
+      _transfer2 =
+        money_transfer_fixture(
+          amount: Money.new(300, :EUR),
+          book_id: book.id,
+          tenant_id: member2.id,
+          peers: [
+            %{member_id: member1.id},
+            %{member_id: member2.id},
+            %{member_id: member3.id}
+          ]
+        )
+
+      assert Balance.for_book(book.id) == %{
+               members_balance: %{
+                 member1.id => Money.new(100, :EUR),
+                 member2.id => Money.new(100, :EUR),
+                 member3.id => Money.new(-200, :EUR)
+               },
+               transactions: [
+                 %{from: member3.id, to: member2.id, amount: Money.new(100, :EUR)},
+                 %{from: member3.id, to: member1.id, amount: Money.new(100, :EUR)}
                ]
              }
     end
