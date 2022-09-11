8de1d021ca16002e90c5438ee4f4c1ccc65f76bd
Enzo <enzo.baldisserri@outlook.fr>
2022-09-11 23:17:57 +0200

    chore: update dependencies (#46)

--- apps/app/mix.exs
+++ apps/app/mix.exs
@@ -41,7 +41,7 @@ defmodule App.MixProject do
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
       {:jason, "~> 1.2"},
-      {:swoosh, "~> 1.7.4"},
+      {:swoosh, "~> 1.8"},
       {:gen_smtp, "~> 1.0"},
       {:hackney, "~> 1.18"},
       {:decimal, "~> 2.0"},
