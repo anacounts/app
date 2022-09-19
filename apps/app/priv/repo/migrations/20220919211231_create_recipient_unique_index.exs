d08ce3ff8c1d9f21d7f3898103e492a64860ce17
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2022-09-19 23:26:09 +0200

    fix: forbid sending multiple times a notification to a user

---
+++ apps/app/priv/repo/migrations/20220919211231_create_recipient_unique_index.exs
@@ -0,0 +1,10 @@
+defmodule App.Repo.Migrations.CreateRecipientUniqueIndex do
+  use Ecto.Migration
+
+  @disable_ddl_transaction true
+  @disable_migration_lock true
+
+  def change do
+    create unique_index(:notification_recipients, [:notification_id, :user_id], concurrently: true)
+  end
+end
