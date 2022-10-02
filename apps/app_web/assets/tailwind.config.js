fbcb8481b4ac54bf458039ddd461d9538acc9672
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2022-10-02 16:11:35 +0200

    feat: enhance books index page

--- apps/app_web/assets/tailwind.config.js
+++ apps/app_web/assets/tailwind.config.js
@@ -53,6 +53,7 @@ module.exports = {
   },
   plugins: [
     require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
     plugin(({ addVariant }) =>
       addVariant("phx-no-feedback", ["&.phx-no-feedback", ".phx-no-feedback &"])
     ),
