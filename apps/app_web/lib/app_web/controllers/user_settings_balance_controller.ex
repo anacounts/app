9d2e62a7764828063b347b6eac89698db47604a4
Enzo <enzo.baldisserri@outlook.fr>
2022-10-16 12:09:52 +0200

    refactor: update controller & view

--- apps/app_web/lib/app_web/controllers/user_settings_balance_controller.ex
+++ apps/app_web/lib/app_web/controllers/user_settings_balance_controller.ex
@@ -3,48 +3,32 @@ defmodule AppWeb.UserSettingsBalanceController do
 
   alias App.Balance
 
-  plug(:assign_changesets)
+  plug :assign_user_config
 
   def edit(conn, _params) do
     render(conn, "edit.html", page_title: gettext("Balance Settings"))
   end
 
-  def update(conn, %{"user_params" => user_params}) do
-    # TODO Maybe user/user_id should be the first param of `upsert_user_params`
-    params = Map.put(user_params, "user_id", conn.assigns.current_user.id)
-
-    case Balance.upsert_user_params(params) do
-      {:ok, _user_params} ->
+  def update(conn, %{"user_config" => user_config_params}) do
+    case Balance.update_user_config(conn.assigns.user_config, user_config_params) do
+      {:ok, _user_config} ->
         conn
         |> put_flash(:info, gettext("Balance settings updated"))
         |> redirect(to: Routes.user_settings_balance_path(conn, :edit))
 
       {:error, changeset} ->
-        render(conn, "edit.html", %{
-          :page_title => gettext("Balance Settings"),
-          String.to_existing_atom("#{user_params["means_code"]}_changeset") => changeset
-        })
+        render(conn, "edit.html",
+          page_title: gettext("Balance Settings"),
+          changeset: changeset
+        )
     end
   end
 
-  # TODO Does not belong here
-  @default_user_params [%{means_code: :weight_by_income, params: nil, user_id: 0}]
+  defp assign_user_config(conn, _opts) do
+    user_config = Balance.get_user_config_or_default(conn.assigns.current_user)
 
-  defp assign_changesets(conn, _opts) do
-    # TODO There MUST be a better way to do this
-    user_params =
-      Balance.list_user_params(conn.assigns.current_user.id)
-      |> Enum.map(&Map.from_struct/1)
-      |> Kernel.++(@default_user_params)
-      |> Enum.uniq_by(& &1.means_code)
-
-    for user_param <- user_params, reduce: conn do
-      conn ->
-        assign(
-          conn,
-          String.to_existing_atom("#{user_param.means_code}_changeset"),
-          Balance.UserParams.changeset(user_param)
-        )
-    end
+    conn
+    |> assign(:user_config, user_config)
+    |> assign(:changeset, Balance.user_config_change(user_config))
   end
 end
