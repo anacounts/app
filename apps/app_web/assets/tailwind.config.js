089f24e72fa532370b3756160558105ff99fca25
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2022-10-30 18:13:28 +0100

    feat: rework navbar

--- apps/app_web/assets/tailwind.config.js
+++ apps/app_web/assets/tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
     colors: {
       // Theme colors
       theme: {
-        darker: "#e63700",
+        darker: "#cc3000",
         DEFAULT: "#ff3d00",
         lighter: "#ff501a",
         contrast: "#ffffff",
