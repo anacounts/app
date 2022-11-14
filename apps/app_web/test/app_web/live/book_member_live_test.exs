d8468d7d2a49db992677d6820841d9c1e13ef3c0
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2022-11-14 23:15:03 +0100

    test: make tests more precise

--- apps/app_web/test/app_web/live/book_member_live_test.exs
+++ apps/app_web/test/app_web/live/book_member_live_test.exs
@@ -2,17 +2,29 @@ defmodule AppWeb.BookMemberLiveTest do
   use AppWeb.ConnCase
 
   import Phoenix.LiveViewTest
+  import App.AuthFixtures
   import App.BooksFixtures
   import App.Books.MembersFixtures
 
   describe "Index" do
     setup [:register_and_log_in_user, :book_with_member_context]
 
-    test "displays book", %{conn: conn, book: book} do
+    test "displays book members", %{conn: conn, book: book} do
+      _member = book_member_fixture(book, user_fixture(display_name: "Samuel"))
+
       {:ok, _show_live, html} = live(conn, Routes.book_member_index_path(conn, :index, book))
 
-      assert html =~ "Details"
-      assert html =~ book.name
+      # the book name is the main title
+      assert html =~ book.name <> "\n</h1>"
+      # the tabs are displayed
+      assert html =~ "Members"
+      # there is a link to go to the invitations page
+      assert html =~ ~s{href="#{Routes.invitation_index_path(conn, :index, book)}"}
+      # the member is displayed, along with its balance and join status
+      # FIXME It's not possible to set the `display_name` in the fixture
+      # assert html =~ "Samuel"
+      assert html =~ Money.new(0, :EUR) |> Money.to_string()
+      assert html =~ "Joined"
     end
 
     test "deletes book", %{conn: conn, book: book} do
