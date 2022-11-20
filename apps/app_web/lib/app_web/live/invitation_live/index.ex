8e58a80a92b1785d1d562258bb026a30b874817a
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2022-11-20 23:25:45 +0100

    feat: add suggestions to invitation page

--- apps/app_web/lib/app_web/live/invitation_live/index.ex
+++ apps/app_web/lib/app_web/live/invitation_live/index.ex
@@ -7,35 +7,36 @@ defmodule AppWeb.InvitationLive.Index do
   use AppWeb, :live_view
 
   alias App.Auth.Avatars
+  alias App.Books
   alias App.Books.Members
 
   on_mount {AppWeb.BookAccess, :ensure_book!}
 
   @impl Phoenix.LiveView
   def mount(_params, _session, socket) do
-    book = socket.assigns.book
-    members = Members.list_members_of_book(book)
+    %{book: book, current_user: current_user} = socket.assigns
+    invitations_suggestions = Books.invitations_suggestions(book, current_user)
 
     socket =
       assign(socket,
         page_title: gettext("Invitations · %{book_name}", book_name: book.name),
-        members: members
+        invitations_suggestions: invitations_suggestions
       )
 
-    {:ok, socket}
+    {:ok, socket, temporary_assigns: [invitations_suggestions: []]}
   end
 
   @impl Phoenix.LiveView
-  def handle_event("invite", %{"email" => email}, socket) do
-    book = socket.assigns.book
+  def handle_event("invite_email", %{"email" => email}, socket) do
+    %{book: book, current_user: current_user} = socket.assigns
 
     # TODO Handle errors (e.g. invited user is already a member)
     # TODO Do not allow access to the page if not allowed to invite people
-    {:ok, member} = Members.invite_new_member(book.id, socket.assigns.current_user, email)
+    {:ok, _member} = Members.invite_new_member(book.id, current_user, email)
 
     {:noreply,
      socket
-     |> put_flash(:info, gettext("Invitation sent successfully"))
-     |> update(:members, &[member | &1])}
+     |> assign(invitations_suggestions: Books.invitations_suggestions(book, current_user))
+     |> put_flash(:info, gettext("Invitation sent successfully"))}
   end
 end
