cce450ebf74c8b0bb01e1e34bb14e0cd9a842c8d
Enzo <enzo.baldisserri@outlook.fr>
2022-12-04 16:05:30 +0100

    chore: setup sentry (#137)

--- apps/app_web/mix.exs
+++ apps/app_web/mix.exs
@@ -54,6 +54,7 @@ defmodule AppWeb.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:app, in_umbrella: true},
+      {:sentry, "~> 8.0"},
       {:jason, "~> 1.4"},
       {:earmark, "~> 1.4"},
       {:plug_cowboy, "~> 2.5"}
