0255d1ae84bdd804d04d534ba1bc4a72545c7839
Enzo <enzo.baldisserri@outlook.fr>
2023-04-01 14:51:39 +0200

    ci: put back `:releases` key in MixProject (#153)

--- mix.exs
+++ mix.exs
@@ -7,7 +7,15 @@ defmodule Anacounts.Umbrella.MixProject do
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      aliases: aliases()
+      aliases: aliases(),
+      releases: [
+        app_web: [
+          applications: [
+            app: :permanent,
+            app_web: :permanent
+          ]
+        ]
+      ]
     ]
   end
 
