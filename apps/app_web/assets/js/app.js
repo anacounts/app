5edfd14826121ed49522f28d1d977536d64985b8
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2023-07-09 18:46:56 +0200

    feat: create `panel-group` component

--- apps/app_web/assets/js/app.js
+++ apps/app_web/assets/js/app.js
@@ -21,6 +21,8 @@ import "phoenix_html";
 import { Socket } from "phoenix";
 import { LiveSocket } from "phoenix_live_view";
 import topbar from "../vendor/topbar";
+// Set up the JavaScript dedicated to page components, defined in AppWeb.PageComponents.
+import "./page_components";
 
 let csrfToken = document
   .querySelector("meta[name='csrf-token']")
