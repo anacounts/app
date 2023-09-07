8d6c15647aecacc2e94a2efdbf5ff9db821aeab0
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2023-09-07 20:18:09 +0200

    feat: add `<.flash>` and `<.flash_group>` components

--- apps/app_web/lib/app_web/components/core_components.ex
+++ apps/app_web/lib/app_web/components/core_components.ex
@@ -324,6 +324,89 @@ defmodule AppWeb.CoreComponents do
   end
 
   @doc """
+  Renders flash notices.
+
+  ## Examples
+
+      <.flash kind={:info} flash={@flash} />
+      <.flash kind={:info} phx-mounted={show("#flash")}>Welcome Back!</.flash>
+  """
+  attr :id, :string, doc: "the optional id of flash container"
+  attr :flash, :map, default: %{}, doc: "the map of flash messages to display"
+  attr :title, :string, default: nil
+  attr :kind, :atom, values: [:info, :error], doc: "used for styling and flash lookup"
+  attr :rest, :global, doc: "the arbitrary HTML attributes to add to the flash container"
+
+  slot :inner_block, doc: "the optional inner block that renders the flash message"
+
+  def flash(assigns) do
+    assigns = assign_new(assigns, :id, fn -> "flash-#{assigns.kind}" end)
+
+    ~H"""
+    <div
+      :if={msg = render_slot(@inner_block) || Phoenix.Flash.get(@flash, @kind)}
+      id={@id}
+      phx-click={JS.push("lv:clear-flash", value: %{key: @kind}) |> hide("##{@id}")}
+      role="alert"
+      class={["flash", @kind == :info && "flash--info", @kind == :error && "flash--error"]}
+      {@rest}
+    >
+      <p :if={@title} class="flash__title">
+        <.icon :if={@kind == :info} name="info" class="flash__icon" />
+        <.icon :if={@kind == :error} name="error" class="flash__icon" />
+        <%= @title %>
+      </p>
+      <p class="flash__body"><%= msg %></p>
+      <button type="button" class="group flash__close-button" aria-label={gettext("Close")}>
+        <.icon name="close" class="flash__close-icon" />
+      </button>
+    </div>
+    """
+  end
+
+  @doc """
+  Shows the flash group with standard titles and content.
+
+  ## Examples
+
+      <.flash_group flash={@flash} />
+  """
+  attr :flash, :map, required: true, doc: "the map of flash messages"
+  attr :id, :string, default: "flash-group", doc: "the optional id of flash container"
+
+  def flash_group(assigns) do
+    ~H"""
+    <div id={@id}>
+      <.flash kind={:info} title={gettext("Success!")} flash={@flash} />
+      <.flash kind={:error} title={gettext("Error!")} flash={@flash} />
+      <.flash
+        id="client-error"
+        kind={:error}
+        title={gettext("We can't find the internet")}
+        phx-disconnected={show(".phx-client-error #client-error")}
+        phx-connected={hide("#client-error")}
+        hidden
+      >
+        <%= gettext("Attempting to reconnect") %>
+        <.icon name="autorenew" class="ml-1 h-4 w-4 animate-spin" />
+      </.flash>
+
+      <.flash
+        id="server-error"
+        kind={:error}
+        title={gettext("Something went wrong!")}
+        phx-disconnected={show(".phx-server-error #server-error")}
+        phx-connected={hide("#server-error")}
+        hidden
+      >
+        <%= gettext("Hang in there while we get back on track") %>
+        <.icon name="autorenew" class="ml-1 h-4 w-4 animate-spin" />
+      </.flash>
+    </div>
+    """
+  end
+
+  @doc """
   Generates a heading element.
 
   [INSERT LVATTRDOCS]
@@ -860,6 +943,29 @@ defmodule AppWeb.CoreComponents do
       <%= render_slot(@inner_block) %>
     </p>
     """
+  end
+
+  ## JS Commands
+
+  def show(js \\ %JS{}, selector) do
+    JS.show(js,
+      to: selector,
+      transition:
+        {"transition-all transform ease-out duration-300",
+         "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95",
+         "opacity-100 translate-y-0 sm:scale-100"}
+    )
+  end
+
+  def hide(js \\ %JS{}, selector) do
+    JS.hide(js,
+      to: selector,
+      time: 200,
+      transition:
+        {"transition-all transform ease-in duration-200",
+         "opacity-100 translate-y-0 sm:scale-100",
+         "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"}
+    )
   end
 
   @doc """
