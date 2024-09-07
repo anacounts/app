03fd84d1e3006362153e80e4e442cf61000e2811
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2024-09-07 23:45:02 +0200

    chore: make font-family work in psb-sandbox

--- apps/app_web/lib/app_web/storybook.ex
+++ apps/app_web/lib/app_web/storybook.ex
@@ -7,7 +7,7 @@ defmodule AppWeb.Storybook do
     otp_app: :app_web,
     content_path: Path.expand("../../storybook", __DIR__),
     # assets path are remote path, not local file-system paths
-    css_path: "/assets/app.css",
+    css_path: "/assets/storybook.css",
     js_path: "/assets/storybook.js",
     sandbox_class: "app-web",
     title: "Anacounts Storybook",
