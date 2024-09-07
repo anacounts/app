457f08c63e9f28d6490d4013a17de1b80b947e4e
Enzo <enzo.baldisserri@outlook.fr>
2024-09-08 00:30:25 +0200

    feat(v2): add <.alert> component (#246)

---
+++ apps/app_web/storybook/core_components/alert.story.exs
@@ -0,0 +1,33 @@
+defmodule Storybook.CoreComponents.Alert do
+  use PhoenixStorybook.Story, :component
+
+  def function, do: &AppWeb.CoreComponents.alert/1
+
+  def imports, do: [{AppWeb.CoreComponents, icon: 1}]
+
+  def variations do
+    [
+      %Variation{
+        id: :error,
+        attributes: %{
+          kind: :error,
+          style: "width: 20rem"
+        },
+        slots: [~s|Some information is missing to balance the book|]
+      },
+      %Variation{
+        id: :warning,
+        attributes: %{
+          kind: :warning,
+          style: "width: 20rem"
+        },
+        slots: [
+          """
+          <span class="psb-grow">Your revenues are not set</span>
+          <.icon name={:chevron_right} />
+          """
+        ]
+      }
+    ]
+  end
+end
