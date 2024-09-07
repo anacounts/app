ecf96c193a343e35684b600e996b2eeccb097bc7
Enzo <enzo.baldisserri@outlook.fr>
2024-09-08 00:30:25 +0200

    feat(v2): add <.alert> component (#246)

--- apps/app_web/assets/tailwind.config.js
+++ apps/app_web/assets/tailwind.config.js
@@ -7,66 +7,79 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./js/**/*.js", "../lib/*_web.ex", "../lib/*_web/**/*.*ex"],
   theme: {
-    colors: {
-      // Theme colors
-      theme: {
-        50: "#ffede8",
-        100: "#ffcab9",
-        200: "#ffa78b",
-        300: "#ff845d",
-        400: "#ff602e",
-        500: "#ff3d00",
-        600: "#d13200",
-        700: "#a22700",
-        800: "#741c00",
-        900: "#461100",
-        950: "#170600",
-        // TODO deprecated shades, to be removed
-        darker: "#cc3000",
-        DEFAULT: "#ff3d00",
-        lighter: "#ff501a",
-        contrast: "#ffffff",
-      },
-      // Action colors
-      action: {
-        DEFAULT: "#485fc7",
-      },
-      // Gray colors
-      white: "#ffffff",
-      gray: {
-        50: "#f3f3f4",
-        100: "#dadcde",
-        200: "#c1c5c9",
-        300: "#a9aeb3",
-        400: "#90979d",
-        500: "#778088",
-        600: "#62686f",
-        700: "#4c5156",
-        800: "#363a3e",
-        900: "#212325",
-        950: "#0b0c0c",
-        // TODO deprecated shades, to be removed
-        5: "#f8f9fa",
-        10: "#f1f3f4",
-        20: "#e9ecef",
-        30: "#dadce0",
-        40: "#c6c8ce",
-        50: "#9aa0a6",
-        60: "#80868b",
-        70: "#5f6368",
-        80: "#3c4043",
-        90: "#202124",
-      },
-      black: "#000000",
-      background: "#f8f9fa",
-      // Status colors
-      info: "#0b61ec",
-      error: "#cd0000",
-      // Helper colors
-      transparent: "transparent",
-      current: "currentColor",
+    borderRadius: {
+      component: "0.5rem",
+      container: "1rem",
+      full: "9999px",
+
+      // TODO(v2,end) remove default border radius values
+      DEFAULT: "0.25rem",
+      md: "0.375rem",
+      lg: "0.5rem",
+      "3xl": "1.5rem",
     },
     extend: {
+      colors: {
+        // Theme colors
+        theme: {
+          50: "#ffede8",
+          100: "#ffcab9",
+          200: "#ffa78b",
+          300: "#ff845d",
+          400: "#ff602e",
+          500: "#ff3d00",
+          600: "#d13200",
+          700: "#a22700",
+          800: "#741c00",
+          900: "#461100",
+          950: "#170600",
+          // TODO deprecated shades, to be removed
+          darker: "#cc3000",
+          DEFAULT: "#ff3d00",
+          lighter: "#ff501a",
+          contrast: "#ffffff",
+        },
+
+        // TODO(v2,end) remove all custom colors but `theme`
+        // Action colors
+        action: {
+          DEFAULT: "#485fc7",
+        },
+        // Gray colors
+        white: "#ffffff",
+        gray: {
+          50: "#f3f3f4",
+          100: "#dadcde",
+          200: "#c1c5c9",
+          300: "#a9aeb3",
+          400: "#90979d",
+          500: "#778088",
+          600: "#62686f",
+          700: "#4c5156",
+          800: "#363a3e",
+          900: "#212325",
+          950: "#0b0c0c",
+          // TODO deprecated shades, to be removed
+          5: "#f8f9fa",
+          10: "#f1f3f4",
+          20: "#e9ecef",
+          30: "#dadce0",
+          40: "#c6c8ce",
+          50: "#9aa0a6",
+          60: "#80868b",
+          70: "#5f6368",
+          80: "#3c4043",
+          90: "#202124",
+        },
+        black: "#000000",
+        background: "#f8f9fa",
+        // Status colors
+        info: "#0b61ec",
+        error: "#cd0000",
+        // Helper colors
+        transparent: "transparent",
+        current: "currentColor",
+      },
       height: {
         md: "2.5rem",
       },
