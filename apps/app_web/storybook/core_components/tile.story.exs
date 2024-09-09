76d855aecf75429749224009bdee3a8bc57cfd7e
Enzo <enzo.baldisserri@outlook.fr>
2024-09-10 00:33:51 +0200

    feat: add <.tile> core component (#260)

---
+++ apps/app_web/storybook/core_components/tile.story.exs
@@ -0,0 +1,63 @@
+defmodule Storybook.CoreComponents.Tile do
+  use PhoenixStorybook.Story, :component
+
+  def function, do: &AppWeb.CoreComponents.tile/1
+
+  def container, do: {:div, "data-background": "theme"}
+  def imports, do: [{AppWeb.CoreComponents, button: 1, icon: 1}]
+
+  def variations do
+    [
+      %Variation{
+        id: :book,
+        attributes: %{
+          style: "width: 20rem"
+        },
+        slots: [
+          """
+          <span class="label leading-none line-clamp-2">Milano</span>
+          <.button kind={:ghost}>
+            Open
+            <.icon name={:chevron_right} />
+          </.button>
+          """
+        ]
+      },
+      %Variation{
+        id: :balance,
+        attributes: %{
+          style: "width: 20rem"
+        },
+        slots: [
+          """
+          <div class="grid grid-rows-2 grid-cols-[1fr_1fr] items-center grid-flow-col">
+            <div class="truncate">
+              <span class="label text-theme-500">John Doe</span>
+              owes
+              <span class="label">Jane Doe</span>
+            </div>
+            <span class="label">330€</span>
+            <.button kind={:ghost} class="row-span-2">
+              Settle up
+              <.icon name={:chevron_right} />
+            </.button>
+          </div>
+          """
+        ]
+      },
+      %Variation{
+        id: :new_transfer,
+        attributes: %{
+          color: :primary,
+          style: "width: 20rem"
+        },
+        slots: [
+          """
+          <.icon name={:plus} />
+          New transfer
+          """
+        ]
+      }
+    ]
+  end
+end
