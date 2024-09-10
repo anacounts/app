fa97e350eb50d1bdb57d22a773d6690a17525878
Enzo <enzo.baldisserri@outlook.fr>
2024-09-10 09:47:27 +0200

    feat: add `<.transfer_tile>` transfer component

---
+++ apps/app_web/storybook/transfers_components/_transfers_components.index.exs
@@ -0,0 +1,8 @@
+defmodule Storybook.TransfersComponents do
+  # See https://hexdocs.pm/phoenix_storybook/PhoenixStorybook.Index.html for full index
+  # documentation.
+
+  use PhoenixStorybook.Index
+
+  def folder_icon, do: {:hero, "arrows_right_left"}
+end
